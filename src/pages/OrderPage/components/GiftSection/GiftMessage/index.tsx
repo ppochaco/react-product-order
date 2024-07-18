cd8f886c4e955cf3263701088d07e223fcd0a1fd
ajin <aj5210@naver.com>
2024-07-18 17:05:42 +0900

    feat(orderPage): 메세지 입력에 글자수 UI 추가

--- src/pages/OrderPage/components/GiftSection/GiftMessage/index.tsx
+++ src/pages/OrderPage/components/GiftSection/GiftMessage/index.tsx
@@ -4,7 +4,12 @@ import { Container } from '@/components/ui/Layout/Container';
 import { Text } from '@/components/ui/Text';
 import { TextArea } from '@/components/ui/TextArea';
 
-import { containerStyle, textAreaStyle } from './styles';
+import {
+  containerStyle,
+  textAreaContainerStyle,
+  textAreaStyle,
+  textLengthStyle,
+} from './styles';
 
 type GiftMessageProps = {
   formData: OrderFormType;
@@ -15,6 +20,8 @@ export const GiftMessage = ({
   formData,
   handleInputChange,
 }: GiftMessageProps) => {
+  const textLength = formData.gitfMessage.length;
+
   return (
     <Container
       flexDirection="column"
@@ -25,12 +32,15 @@ export const GiftMessage = ({
       <Text size="lg" isBold>
         나에게 주는 선물
       </Text>
-      <TextArea
-        placeholder="선물과 함께 보낼 메세지를 적어보세요"
-        value={formData.gitfMessage}
-        onChange={(e) => handleInputChange('gitfMessage', e.target.value)}
-        css={textAreaStyle}
-      />
+      <div css={textAreaContainerStyle}>
+        <TextArea
+          placeholder="선물과 함께 보낼 메세지를 적어보세요"
+          value={formData.gitfMessage}
+          onChange={(e) => handleInputChange('gitfMessage', e.target.value)}
+          css={textAreaStyle}
+        />
+        <span css={textLengthStyle(textLength > 100)}>{textLength}/100</span>
+      </div>
     </Container>
   );
 };
--- src/pages/OrderPage/components/GiftSection/GiftMessage/styles.ts
+++ src/pages/OrderPage/components/GiftSection/GiftMessage/styles.ts
@@ -1,9 +1,33 @@
-import { css } from '@emotion/react';
+import { CSSObject, css } from '@emotion/react';
+
+import { colors } from '@/styles/variants/theme';
 
 export const containerStyle = css({
   padding: '3rem 4rem',
 });
 
+export const textAreaContainerStyle = css({
+  position: 'relative',
+  width: '100%',
+});
+
 export const textAreaStyle = css({
   height: '6.5rem',
 });
+
+export const textLengthStyle = (isMaxLength: boolean) => {
+  const defaultStyle: CSSObject = {
+    position: 'absolute',
+    bottom: '-1.2rem',
+    right: '0',
+  };
+
+  if (isMaxLength) {
+    return css({
+      ...defaultStyle,
+      color: colors.red,
+    });
+  }
+
+  return css({ ...defaultStyle });
+};
